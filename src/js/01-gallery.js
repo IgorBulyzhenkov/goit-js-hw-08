67d0b9c9369c861c1df74ab13760f741396cc9e2
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-06-01 22:09:49 +0300

    src

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+// // Add imports above this line
+// import { galleryItems } from './gallery-items';
+// // Change code below this line
+
+// console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+const cardsImage = createImageMarkup(galleryItems);
+
+galleryEl.innerHTML = cardsImage;
+
+function createImageMarkup(img) {
+  return img
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+let Lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: '.gallery__image',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  animationSpeed: 250,
+  preloading: false,
+  docClose: false,
+  widthRatio: 1,
+  doubleTapZoom: 1.5,
+});
