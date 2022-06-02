e27df53d4d2f5e8b62a683eae9395a1702c8ef92
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-06-02 17:09:49 +0300

    src

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+const throttle = require("lodash.throttle");
+const LOCAL_TIME = 'videoplayer-current-time';
 
+
+let iframe = document.querySelector('iframe');
+let player = new Vimeo.Player(iframe);
+
+console.log(player);
+
+player.on('timeupdate', throttle(playTime,1000));
+
+function playTime({seconds}) {
+    console.log('played the video!');
+    localStorage.setItem(LOCAL_TIME, seconds)
+
+}
+
+const savedTime = localStorage.getItem(LOCAL_TIME);
+
+
+player.setCurrentTime(savedTime);
