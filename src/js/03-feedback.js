18918a1e729f72feddb67611bc37cc33ce26c4da
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-06-03 15:03:12 +0300

    src

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,9 @@
+//Сделал импорт из Storage
+import Storage from './storage';
 //сделал переменные
 const form = document.querySelector('.feedback-form');
 
 // Сделал ключ для локального хранилища
-
 const STORAGE_KEY = 'feedback-form-state';
 
 // Вызов функции хранилища
@@ -10,36 +11,31 @@ getTextLocalStorage();
 
 //Повесил слушатилей на форму
 //Беру значения  из формы
-
 form.addEventListener('submit', e => {
   e.preventDefault();
   const formData = new FormData(form);
   formData.forEach((name, value) => console.log(`${value} : ${name}`));
   form.reset();
-  localStorage.removeItem(STORAGE_KEY);
+  Storage.remove(STORAGE_KEY);
 });
 
 //запюисываю текст в локальное хранилище
-
 form.addEventListener('change', e => {
   const { name, value } = e.target;
 
-  let saveData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
+  let saveData = Storage.load(STORAGE_KEY) ?? {};
   saveData[name] = value;
 
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(saveData));
+  Storage.save(STORAGE_KEY, saveData);
 });
 
 //Получаю значение из хранилища
-
 function getTextLocalStorage() {
-  let saveData = localStorage.getItem(STORAGE_KEY);
+  let saveData = Storage.load(STORAGE_KEY);
 
+  console.log(saveData);
   if (!saveData) return;
-
-  saveData = JSON.parse(saveData);
-  Object.entries(saveData).forEach(([name, value]) => {
-    form.elements[name].value = value;
-  });
-  
+  Object.entries(saveData).forEach(
+    ([name, value]) => (form.elements[name].value = value)
+  );
 }
