ed775e66aa4709ebe0457bb1b4ec0f1fc287ceaa
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-06-03 10:25:34 +0300

    src

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+//сделал переменные
+const form = document.querySelector('.feedback-form');
 
+// Сделал ключ для локального хранилища
+
+const STORAGE_KEY = 'feedback-form-state';
+
+// Вызов функции хранилища
+getTextLocalStorage();
+
+//Повесил слушатилей на форму
+//Беру значения  из формы
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  formData.forEach((name, value) => console.log(`${value} : ${name}`));
+  form.reset();
+  localStorage.removeItem(STORAGE_KEY);
+});
+
+//запюисываю текст в локальное хранилище
+
+form.addEventListener('change', e => {
+  const { name, value } = e.target;
+
+  let saveData = JSON.parse(localStorage.getItem(STORAGE_KEY)) ?? {};
+  saveData[name] = value;
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(saveData));
+});
+
+//Получаю значение из хранилища
+
+function getTextLocalStorage() {
+  let saveData = localStorage.getItem(STORAGE_KEY);
+
+  if (!saveData) return;
+
+  saveData = JSON.parse(saveData);
+  Object.entries(saveData).forEach(([name, value]) => {
+    form.elements[name].value = value;
+  });
+  
+}
