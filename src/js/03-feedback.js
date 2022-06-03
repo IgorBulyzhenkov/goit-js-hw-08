121039556b54ace82be83b40597425c0e6030d1f
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2022-06-03 15:18:56 +0300

    src

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,4 @@
+const throttle = require('lodash.throttle');
 //Сделал импорт из Storage
 import Storage from './storage';
 //сделал переменные
@@ -10,24 +11,30 @@ const STORAGE_KEY = 'feedback-form-state';
 getTextLocalStorage();
 
 //Повесил слушатилей на форму
-//Беру значения  из формы
-form.addEventListener('submit', e => {
+form.addEventListener('change', throttle(textSave,500));
+form.addEventListener('submit', getTextFromForm);
+
+//Беру текст  из формы
+function getTextFromForm(e) {
   e.preventDefault();
+
   const formData = new FormData(form);
   formData.forEach((name, value) => console.log(`${value} : ${name}`));
+
   form.reset();
+
   Storage.remove(STORAGE_KEY);
-});
+}
 
 //запюисываю текст в локальное хранилище
-form.addEventListener('change', e => {
+function textSave(e) {
   const { name, value } = e.target;
 
   let saveData = Storage.load(STORAGE_KEY) ?? {};
   saveData[name] = value;
 
   Storage.save(STORAGE_KEY, saveData);
-});
+}
 
 //Получаю значение из хранилища
 function getTextLocalStorage() {
